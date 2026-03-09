9c301eabdc5e51b97dedba10da9b11f20f007067
Ahmad Raza <cathode1122@gmail.com>
2026-03-09 10:43:47 +0500

    Time: 7 ms (27.37%), Space: 23 MB (51.1%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        for(int i = 0; i < 9; i++) {
+            set<char> row;
+            set<char> col;
+            set<char> box;
+
+            for(int j = 0; j < 9; j++) {
+
+                if(board[i][j] != '.') {
+                    if(row.count(board[i][j]))
+                        return false;
+                    row.insert(board[i][j]);
+                }
+
+                if(board[j][i] != '.') {
+                    if(col.count(board[j][i]))
+                        return false;
+                    col.insert(board[j][i]);
+                }
+
+                int r = 3 * (i / 3) + j / 3;
+                int c = 3 * (i % 3) + j % 3;
+
+                if(board[r][c] != '.') {
+                    if(box.count(board[r][c]))
+                        return false;
+                    box.insert(board[r][c]);
+                }
+            }
+        }
+
+        return true;
+    }
+};
