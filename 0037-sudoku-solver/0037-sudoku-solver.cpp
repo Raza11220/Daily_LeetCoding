78e5c07f18a09a77d5ab08f1ea6c4a7096c7dda1
Ahmad Raza <cathode1122@gmail.com>
2026-03-18 11:16:42 +0500

    Time: 312 ms (60.8%), Space: 8.9 MB (10.17%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+
+        // Check row & column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) return false;
+            if (board[i][col] == num) return false;
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, i, j, num)) {
+
+                            board[i][j] = num;
+
+                            if (solve(board))
+                                return true;
+
+                            // Backtrack
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
